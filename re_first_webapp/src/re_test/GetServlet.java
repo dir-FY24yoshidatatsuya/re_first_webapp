5242fbb3c19a79bd7365cfac13de7a7c6252692a
dir-FY24yoshidatatsuya <TatsuyaYoshida()@Dirbato-1341x>
2024-04-18 10:53:15 +0900

    zenbu

--- re_first_webapp/src/re_test/GetServlet.java
+++ re_first_webapp/src/re_test/GetServlet.java
@@ -6,18 +6,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 
 /**
  * Servlet implementation class HelloServlet
  */
 @WebServlet("/HelloServlet")
-public class HelloServlet extends HttpServlet {
+public class GetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloServlet() {
+    public GetServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -25,19 +26,16 @@ public class HelloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	        throws ServletException, IOException {
-	    response.setContentType("text/html; charset=UTF-8");
-	    var out = response.getWriter();
-	    out.println("<html>");
-	    out.println("<head>");
-	    out.println("<title>はじめてのサーブレット</title>");
-	    out.println("</head>");
-	    out.println("<body>");
-	    out.println("こんにちは、サーブレットの世界へ！！");
-	    out.println("</body>");
-	    out.println("</html>");
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String q = request.getParameter("q");
+
+        request.setAttribute("q", q);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/get_result.jsp");
+        rd.forward(request, response);
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
